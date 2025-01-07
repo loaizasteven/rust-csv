d192aac9d37f5312553e321030c61f90f62d7e8e
Steven Loaiza <steven.loaiza@outlook.com>
2025-01-06 22:43:04 -0500

    Update cargo docs

--- docs/src-files.js
+++ docs/src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["sdk",["",[],["lib.rs","reader.rs","stdin_parser.rs"]]],["sdk_usage",["",[],["main.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["sdk",["",[["data",[],["manipulation.rs"]]],["data.rs","lib.rs","reader.rs","stdin_parser.rs"]]],["sdk_usage",["",[],["main.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[56,34]}
+//{"start":36,"fragment_lengths":[97,34]}
