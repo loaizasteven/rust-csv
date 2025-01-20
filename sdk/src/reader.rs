1563fb14f0a0dab0c24ea6943199bde5d2a0d875
Steven Loaiza <steven.loaiza@outlook.com>
2025-01-20 13:24:04 -0500

    Add glob reader

--- sdk/src/reader.rs
+++ sdk/src/reader.rs
@@ -1,7 +1,8 @@
 //! Provide [CsvMetadata] struct and [csv_reader] function to read csv files
-use std::io::BufReader;
 use std::fs::File;
 use clap::Parser;
+use glob::glob;
+use std::io::{SeekFrom, Seek, BufRead, BufReader};
 
 /// A reader module defines the struct containing metadata of the csv file
 /// # Example
@@ -45,4 +46,61 @@ impl CsvMetadata {
 pub fn csv_reader(csv_struct: &CsvMetadata) -> BufReader<File> {
     let f = File::open(&csv_struct.file).expect("Error opening file");
     BufReader::new(f)
+}
+
+fn glob_reader(csv_struct: &CsvMetadata) -> impl Iterator<Item = Result<std::string::String, std::io::Error>>{
+    let mut first_file:bool = true;
+    let mut readers: Vec<BufReader<File>> = Vec::new();
+    let mut pos: u64 = 0;
+
+    for entry in glob(&csv_struct.file).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                println!("{:?}", path.display());
+                if first_file {
+                    let file_base: File = File::open(&path).expect("Error opening file");
+                    let reader = BufReader::new(file_base);
+                    readers.push(reader);
+                    
+                    let file_temp: File = File::open(&path).expect("Error opening file");
+                    let mut temp_reader = BufReader::new(file_temp);
+                    let mut temp_buffer: String = String::new();
+                    temp_reader.read_line(&mut temp_buffer).expect("Unable to read temp file to buffer");
+                    pos = temp_buffer.len() as u64;
+                    first_file = false;
+                }
+                else {
+                    let mut file_cont: File = File::open(&path).expect("Error opening file");
+                    let _ = file_cont.seek(SeekFrom::Start(pos));
+                    let reader = BufReader::new(file_cont);
+                    readers.push(reader);
+                }
+            
+            }
+            Err(e) => eprintln!("{:?}", e),
+        }
+    }
+    // Combine the readers into a single iterator
+    return readers.into_iter().flat_map(|reader| reader.lines())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_glob_reader_multiple_csv() {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR")); //crate root
+        path.push("test/*/*.csv");
+        let csv_handler = CsvMetadata {
+            file: path.to_str().unwrap().to_string(),
+            delimiter: ',',
+            has_header: true,
+            column_types: vec!["string".to_string()]
+        };
+
+        let result = glob_reader(&csv_handler);
+        assert!(result.count() > 0);
+    }
 }
